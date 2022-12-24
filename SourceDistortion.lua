4c34c527bcb29ac11f54d82dc0f3307a845e81db
Isth4t <116909814+Isth4t@users.noreply.github.com>
2022-12-24 12:51:31 +0200

    Update SourceDistortion.lua

--- SourceDistortion.lua
+++ SourceDistortion.lua
@@ -434,8 +434,8 @@ Spawner.runJumpscare = function(config)
     Face.Image = image2
     sound2:Play()
     
-    TS:Create(Face, TweenInfo.new(1.5, Enum.EasingStyle.Linear), {Size = UDim2.new(0, 2000, 0,  2000), ImageTransparency = 0.5}):Play()
-    task.wait(1.5)
+    TS:Create(Face, TweenInfo.new(1, Enum.EasingStyle.Linear), {Size = UDim2.new(0, 2000, 0,  2000), ImageTransparency = 0.5}):Play()
+    task.wait(1)
     JumpscareGui:Destroy()
     
     if sound1 then
