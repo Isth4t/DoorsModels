0a8ede3144bbf1b47c4267d3e44b0294db548814
Isth4t <116909814+Isth4t@users.noreply.github.com>
2022-12-24 12:48:41 +0200

    Update SourceReaper.lua

--- SourceReaper.lua
+++ SourceReaper.lua
@@ -424,8 +424,8 @@ Spawner.runJumpscare = function(config)
     Face.Image = "rbxassetid://11901003953"
     sound2:Play()
     
-    TS:Create(Face, TweenInfo.new(0.5, Enum.EasingStyle.Linear), {Size = UDim2.new(0, 5000, 0,  5000), ImageTransparency = 0.5}):Play()
-    task.wait(0.5)
+    TS:Create(Face, TweenInfo.new(0.25, Enum.EasingStyle.Linear), {Size = UDim2.new(0, 5000, 0,  5000), ImageTransparency = 0.5}):Play()
+    task.wait(0.25)
     JumpscareGui:Destroy()
     
     if sound1 then
